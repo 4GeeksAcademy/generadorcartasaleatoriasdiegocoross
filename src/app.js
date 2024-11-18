964b9f60bcf2d040028078fc0cfd6696fb54addb
Diegocoros <diegocoros22@gmail.com>
2024-11-18 17:33:10 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,51 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+function generarCartaAleatoria() {
+  // Palos y valores de la baraja
+  const palos = ["♥", "♦", "♣", "♠"]; // Corazones, Diamantes, Tréboles, Picas
+  const valores = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+  // Indices aleatorios
+  const randomPaloIndex = Math.floor(Math.random() * palos.length);
+  const randomValorIndex = Math.floor(Math.random() * valores.length);
+
+  // Elegir palo y  valor
+  const palo = palos[randomPaloIndex];
+  const valor = valores[randomValorIndex];
+
+  // Asigna valores a los elementos de la carta
+  const paloClass = palo === "♥" || palo === "♦" ? "red" : "black";
+
+  document.getElementById("top-palos").textContent = palo;
+  document.getElementById("top-palos").className = `top-palos ${paloClass}`;
+
+  document.getElementById("center-number").textContent = valor;
+  document.getElementById(
+    "center-number"
+  ).className = `center-number ${paloClass}`;
+
+  document.getElementById("bottom-palos").textContent = palo;
+  document.getElementById(
+    "bottom-palos"
+  ).className = `bottom-palos ${paloClass}`;
+}
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  // Genera una carta inicialmente
+  generarCartaAleatoria();
+
+  const card = document.querySelector("#card");
+  card.addEventListener("click", generarCartaAleatoria); // Genera nueva carta al hacer clic
 };
